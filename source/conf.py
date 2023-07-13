a05bfa72167ec4b6d423c8b46dcfcafa9ebef289
Shuli Sun <77892089+sunshuli940@users.noreply.github.com>
2023-07-13 19:42:25 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -63,7 +63,7 @@ html_theme = 'sphinx_rtd_theme'
 #     github_repo='SpaGFT',  # Repo name
 #     github_version='master',  # Version
 # )
-# html_static_path = ['_static']
-# html_show_sphinx = False
+html_static_path = ['_static']
+html_show_sphinx = False
 # html_logo = '_static/img/SpaGFT_ LOGO.png'
-# html_style = 'css/style.css'
+html_style = 'css/style.css'
