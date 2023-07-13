1f6eefa709fd922e09aa2ee869e9012856160a55
Shuli Sun <77892089+sunshuli940@users.noreply.github.com>
2023-07-13 17:17:20 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import Command, find_packages, setup
+
+__lib_name__ = "DeepGFT"
+__lib_version__ = "0.0.1"
+__description__ = "clustering spatial transcriptomics data using deep learning and graph Fourier transform"
+__url__ = "https://github.com/jxLiu-bio/DeepGFT"
+__license__ = "MIT"
+
+setup(
+    name = __lib_name__,
+    version = __lib_version__,
+    description = __description__,
+    url = __url__,
+    license = __license__,
+    packages = ['DeepGFT'],
+    install_requires = ["requests"]
+    zip_safe = False,
+    include_package_data = True
+)
