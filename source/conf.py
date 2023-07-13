ce2400db102f7ab5251bf9727662fc046422c4b7
frank <80166502+jxLiu-bio@users.noreply.github.com>
2023-07-13 21:10:35 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -30,15 +30,12 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx_rtd_theme",
-    "nbsphinx",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.mathjax",
-    "myst_parser",
-]
+extensions = ["myst_parser"]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
